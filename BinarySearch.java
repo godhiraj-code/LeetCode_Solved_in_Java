b0542c7053ecdf253be3d10c1d477c063c7aedf1
godhiraj-code <dhirajdas.666@gmail.com>
2021-08-21 10:56:29 +0800

    added order agnosting logic

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,4 +1,4 @@
-package com.das.dhiraj;
+package com.das.dhiraj.LeetCode_Solved_in_Java;
 /*
     When a problem statement has something like sorted array , first thing that should come to your mind
     should be applying Binary Search.
@@ -13,22 +13,34 @@ package com.das.dhiraj;
 public class BinarySearch {
 
     public static void main(String[] args) {
-	    int[] arr = {1, 3, 4, 5, 6, 9};
-        System.out.println(binary_search(arr, 5));
+	   // int[] arr_asc = {1, 3, 4, 5, 6, 9};
+        int[] arr_desc = {9, 8, 7, 5, 4, 3};
+        System.out.println(binary_search(arr_desc, 3));
     }
     // returns the index if target element is found , or -1 if not found.
     static int binary_search(int[] arr, int target){
         int start = 0;
         int end = arr.length - 1;
+        // check if the arr is in ascending order or descending order :
+        boolean isAsc = arr[start] < arr[end];
 
         while(start <= end){
             int mid = start + (end - start)/2;
-            if(target > arr[mid]){
-                start = mid + 1;
-            }else if (target < arr[mid]){
-                end = mid - 1;
-            }else{
+            if (arr[mid] == target){
                 return mid;
+            }
+            if (isAsc) {
+                if (target > arr[mid]) {
+                    start = mid + 1;
+                } else if (target < arr[mid]) {
+                    end = mid - 1;
+                }
+            }else{
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else if (target < arr[mid]) {
+                    start = mid + 1;
+                }
             }
         }
         return  -1;
